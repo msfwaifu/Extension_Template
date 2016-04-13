a372fad5d9a7c4fb7bd4e03566c69c5e7735c66c
Convery <convery10122@gmail.com>
2016-04-13 18:55:15 +0200

    Initial commit

---
+++ Source/DllMain.cpp
@@ -0,0 +1,64 @@
+/*
+    Initial author: (https://github.com/)Convery for Ayria.se
+    License: LGPL 3.0
+    Started: 2016-04-13
+    Notes:
+        The entrypoint for this extension.
+*/
+
+#include <Configuration\All.h>
+#include <cstdarg>
+
+extern "C"
+{
+    // Ayrias extension exports as per the 2016 standard.
+    EXPORT_ATTR void __cdecl onExtensionUnloading(void)
+    {
+    }
+    EXPORT_ATTR void __cdecl onInitializationStart(void)
+    {
+    }
+    EXPORT_ATTR void __cdecl onInitializationComplete(void)
+    {
+    }
+    EXPORT_ATTR void __cdecl onMessage(uint32_t Message, ...)
+    {
+        std::va_list Variadic;
+        va_start(Variadic, Message);
+
+        // Messages are a 32bit FNV1a of a string.
+        switch (Message)
+        {
+        }
+
+        va_end(Variadic);
+    }
+}
+
+#ifdef _WIN32
+#include <Windows.h>
+BOOLEAN WINAPI DllMain(HINSTANCE hDllHandle, DWORD nReason, LPVOID Reserved)
+{
+    switch ( nReason )
+    {
+    case DLL_PROCESS_ATTACH:
+        // Rather not handle all thread updates.
+        DisableThreadLibraryCalls( hDllHandle );
+
+        // Clean the logfile so we only save this session.
+        DeleteLogfile();
+        break;
+    }
+
+    return TRUE;
+}
+
+#else
+
+void __attribute__((constructor)) SOMain()
+{
+    // Clean the logfile so we only save this session.
+    DeleteLogfile();
+}
+
+#endif
---
+++ Source/Utilities/All.h
@@ -0,0 +1,13 @@
+/*
+    Initial author: (https://github.com/)Convery for Ayria.se
+    License: LGPL 3.0
+    Started: 2016-04-13
+    Notes:
+        Includes all utilities, remember to optimize unused ones out.
+*/
+
+#pragma once
+
+#include "Strings\Variadicstring.h"
+#include "Strings\Debugstring.h"
+#include "Files\Filesystem.h"
